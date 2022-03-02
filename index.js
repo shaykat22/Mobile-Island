6859b6e5bd183f6008a45cacce9f3a5e6b85b91b
shaykat <shafinur.rahman.shaykat@gmail.com>
2022-03-02 22:12:48 +0600

    showing display img

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const searchButton = () => {
             .then(data => {
                 if (data.data.length != 0) {
                     phonesDisplay(data)
+                    document.getElementById('display-img').innerHTML = '';
                 }
                 else {
                     error.innerText = "Data not found";
@@ -22,6 +23,8 @@ const searchButton = () => {
             })
         input.value = "";
         error.innerHTML = ""
+        document.getElementById('phone-details').innerHTML = '';
+
     }
 }
 
@@ -35,7 +38,7 @@ const phonesDisplay = (phoneData) => {
         div.classList.add("col-lg-4")
         div.classList.add("mb-5")
         div.innerHTML = `
-        <div class="d-flex justify-content-centerd-flex justify-content-center">
+        <div class="d-flex  justify-content-center">
             <div class="phones  text-light" style="width: 18rem;">
                  <img src="${datas.image}" class="phones-img-top w-50 radius" alt="...">
                  <div class="phones-body">
@@ -59,31 +62,44 @@ const phoneDetails = id => {
             console.log(productDetails);
             const div = document.createElement("div");
             div.classList.add('card')
-            //main.innerHTML = "";
+            singleDetails.innerHTML = "";
             div.innerHTML = `
         
-            <div class="row  ">
-            <div class = 'col-lg-6'>
-            <img src="${productDetails.image}" class="card-img-top w-75 radius img-fluid " alt="...">
-            <h5 id ="phone-text" class="card-title text-light">${productDetails.name}</h5>
-            <p class="card-text text-light">${productDetails.brand}</p> 
-            </div>
-            <div class = "col-lg-6">       
-                    <div class="card-body text-light">                        
-                        <p class="card-text"> <span class="text-title">Chipset:</span> ${productDetails?.releaseDate || 'Release Date Not Decicded'}</p> 
-                        <p class="card-text">${productDetails.mainFeatures?.displaySize || 'Not found'}</p>
-                        <p class="card-text">${productDetails.mainFeatures?.chipSet || 'Not found'}</p>
-                        <p class="card-text">${productDetails.mainFeatures?.sensors || 'Not found'}</p>
+            <div class="row d-flex p-2 ">
+                <div class = 'col-lg-6'>
+                    <img src="${productDetails.image}" class=" w-75  img-fluid " alt="...">
+                    <h5 id ="phone-text" class="card-title ">${productDetails.name}</h5>
+                <p class="card-text ">${productDetails.brand}</p> 
+                </div>
+                 <div class = "col-lg-6">       
+                    <div class="card-body ">                        
+                        <p class="card-text"> <span class="text-title">Release: </span>${productDetails?.releaseDate || 'Release Date Not Decicded'}</p> 
+                        <p class="card-text">Display: </span>${productDetails.mainFeatures?.displaySize || 'Not found'}</p>
+                        <p class="card-text">Chipset:</span>${productDetails.mainFeatures?.chipSet || 'Not found'}</p>
+                        <p class="card-text"><span class="text-title">Sensor: </span>${productDetails.mainFeatures?.sensors || 'Not found'}</p>
                         
                     </div>
                 </div>
             </div>  
             `;
-            for (const prop in productDetails.others) {
-                console.log(prop);
-            }
             singleDetails.appendChild(div);
 
+            const othersDetails = document.createElement('p');
+            if (productDetails.others !== undefined) {
+                phoneDetails.innerHTML = `
+                 <div class ="row";
+                 </div>`
+
+                for (const properties in productDetails.others) {
+                    const othersP = document.createElement('p');
+                    othersP.className = "row"
+                    othersP.innerHTML = `
+                <p class="card-text"><span class='text-title'>Others: </span>${productDetails?.others[properties]}</p>
+                `;
+                    phoneDetails.appendChild(p);
+                };
+
+            }
 
         })
 
